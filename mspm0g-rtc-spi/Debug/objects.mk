1215f8af6ccfec2c9d74b974c2707291f3f67a61
Jason Kridner <jkridner@beagleboard.org>
2024-03-04 20:53:32 -0600

    delete old project

--- mspm0g-rtc-spi/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-l"C:/ti/mspm0_sdk_0_52_00_03_eng/source/ti/driverlib/lib/ticlang/m0p/mspm0g/driverlib.a" -Wl,-llibc.a
-
